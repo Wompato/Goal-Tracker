6295b93b04803fa53986227654a802136a6a419a
alex shelton <alexshelton1999@gmail.com>
2022-02-28 11:54:08 -0500

    Added user routes/controllers and setup jwt authorization/authentication

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,9 +1,10 @@
 const asyncWrapper = require('express-async-wrapper');
 
 const Goal = require('../models/goalModel');
+const User = require('../models/userModel');
 
 const getGoals = asyncWrapper(async (req, res) => {
-    const goals = await Goal.find()
+    const goals = await Goal.find({ user: req.user.id })
 
     res.status(200).json(goals)
 });
@@ -15,7 +16,8 @@ const setGoals = asyncWrapper(async (req, res) => {
     }
 
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        user: req.user.id,
     })
 
     res.status(200).json(goal)
@@ -29,6 +31,20 @@ const updateGoal = asyncWrapper(async (req, res) => {
         throw new Error('Goal not found')
     }
 
+    const user = await User.findById(req.user.id);
+
+    // Check for user
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    // Make sure logged in user matches goal user
+    if(goal.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error('User not authorized')
+    }
+
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
 
     res.status(200).json(updatedGoal)
@@ -40,6 +56,20 @@ const deleteGoal = asyncWrapper(async (req, res) => {
     if(!goal){
         res.status(400)
         throw new Error('Goal not found')
+    }
+
+    const user = await User.findById(req.user.id);
+
+    // Check for user
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    // Make sure logged in user matches goal user
+    if(goal.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error('User not authorized')
     }
 
     await goal.remove()
--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(express.urlencoded({extended: false}))
 const port = process.env.PORT || 3000
 
 app.use('/api/goals', require('./routes/goalRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 
 app.use(errorHandler)
 
